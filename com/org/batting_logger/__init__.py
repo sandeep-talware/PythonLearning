a39e98e415362dcfd502c605d1929f58eaea83f7
Sandeep Talware <34301677+sandeep-talware@users.noreply.github.com>
2021-07-05 12:04:18 +0530

    Update __init__.py

--- com/org/batting_logger/__init__.py
+++ com/org/batting_logger/__init__.py
@@ -1,1 +1,15 @@
 
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    from  .main import main as main_bluprint
+    app.register_blueprint(main_bluprint)
+
+    from .auth import auth as auth_bluprint
+    app.register_blueprint(auth_bluprint)
+
+    return app
+
