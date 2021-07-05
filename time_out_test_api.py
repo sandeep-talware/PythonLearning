ffe5ea0c5478e88e4886701c4b339b68cc4c93ba
Sandeep Talware <34301677+sandeep-talware@users.noreply.github.com>
2021-07-05 11:44:57 +0530

    Delete time_out_test_api.py

--- time_out_test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-try:
-    resp = requests.get(url="https://httpbin.org/delay/5",timeout=5)
-    print(resp.status_code)
-except requests.exceptions.ReadTimeout:
-    print("Timeout Exception")
-finally:
-    print("done")
